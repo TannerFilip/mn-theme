ad1709c6fe5ad36762215ed51c22a55beca9b7b2
Leo McArdle <leomcardle@gmail.com>
2011-08-26 16:12:05 +0100

    cleaned up css and added html5shiv, version 1.0.3

--- header.php
+++ header.php
@@ -5,6 +5,9 @@
 		<link rel="stylesheet" href="<?php bloginfo("template_url"); ?>/css/html5reset-1.6.1.css" type="text/css" media="screen">
 		<link rel="stylesheet" href="<?php bloginfo("template_url"); ?>/fonts/museosans_500_macroman/stylesheet.css" type="text/css" media="screen">
 		<link rel="stylesheet" href="<?php bloginfo("stylesheet_url"); ?>" type="text/css" media="screen">
+		<!--[if lt IE 9]>
+		<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
 		<?php wp_head(); ?>
 	</head>
 	<body>
