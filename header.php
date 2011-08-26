4cf6732cdc966b31b2d7d0ccc845cda3cd5cc9ad
Leo McArdle <leomcardle@gmail.com>
2011-08-26 15:19:25 +0100

    added @font-face, version 1.0.2

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 	<head>
 		<title><?php bloginfo("name"); ?><?php wp_title(); ?></title>
 		<link rel="stylesheet" href="<?php bloginfo("template_url"); ?>/css/html5reset-1.6.1.css" type="text/css" media="screen">
+		<link rel="stylesheet" href="<?php bloginfo("template_url"); ?>/fonts/museosans_500_macroman/stylesheet.css" type="text/css" media="screen">
 		<link rel="stylesheet" href="<?php bloginfo("stylesheet_url"); ?>" type="text/css" media="screen">
 		<?php wp_head(); ?>
 	</head>
